a665e1ecbc637665eff171542f45e2744cf63a67
v1xingyue <qixingyue@126.com>
2023-02-28 23:24:26 +0800

    拆分UI 组件

---
+++ components/themeSelector.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
+
+const ThemeSelector = () => {
+
+    useEffect(() => {
+        themeChange(false);
+    }, [])
+
+    const supportThemes = [
+        "winter", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee"
+    ];
+
+    return (
+        <select className="select select-lg select-bordered w-full max-w-xs" data-choose-theme>
+            {
+                supportThemes.map((name, index) => {
+                    return (<option key={index} value={name}>{name}</option>);
+                })
+            }
+        </select>
+    );
+}
+
+export default ThemeSelector;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/global.css';
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component data-theme="dark"  {...pageProps} />;
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <Navbar />
+        <div className="hero min-h-fit bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <Component data-theme="dark"  {...pageProps} />;
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default MyApp;
